5304241bf7e24fa838153fb7baae995dbf0aa97d
Jonathan Dawson <Jonathan@Jonathans-MacBook.local>
2024-06-21 21:49:06 +0100

    Added some different keys to the gameboard factory

--- src/gameboard-factory.js
+++ src/gameboard-factory.js
@@ -1,5 +1,21 @@
-function newGameboard() {}
-
-function receiveAttack(coords) {}
+function newGameboard() {
+  return {
+    missedAttacks: [],
+    allShipsSunk: false,
+    ships: [],
+    // Each position in array represents a different ship [0] being carrier, [5] being patrol boats etc.
+    shipsToPlace: [1, 2, 3, 4, 5],
+    receiveAttack(coords) {
+      for (let i = 0; i < this.shipPositions.length; i++) {
+        if (
+          coords[0] === this.shipPositions[0] &&
+          coords[1] === this.shipPositions[1]
+        ) {
+          return true;
+        } else return false;
+      }
+    },
+  };
+}
 
 export {};
