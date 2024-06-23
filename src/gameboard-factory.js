eb4e907e9696c8e59fa0bca9d2884230f0516cf7
Jonathan Dawson <jonnydawson@hotmail.com>
2024-06-23 21:56:21 +0100

    Added some addShip functionality to newGameboard

--- src/gameboard-factory.js
+++ src/gameboard-factory.js
@@ -1,10 +1,23 @@
+import { first } from "lodash";
+import { newShip } from "./ship-factory";
+
 function newGameboard() {
   return {
     missedAttacks: [],
     allShipsSunk: false,
     ships: [],
-    // Each position in array represents a different ship [0] being carrier, [5] being patrol boats etc.
-    shipsToPlace: [1, 2, 3, 4, 5],
+    // Each position in array represents a different ship [0](current number 1) being carrier, [4](current number 5) being patrol boats etc.
+    addShip(firstCoord, secondCoord) {
+      const [a, b] = firstCoord;
+      const [x, y] = secondCoord;
+      let length;
+      if (a === x) {
+        length = Math.abs(b - y);
+      } else {
+        length = Math.abs(a - x);
+      }
+      this.ships.push(newShip(length));
+    },
     receiveAttack(coords) {
       for (let i = 0; i < this.shipPositions.length; i++) {
         if (
