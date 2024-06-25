963051dde3aabdbff8ba78297d9ddee26bbd7dfb
Jonathan Dawson <jonnydawson@hotmail.com>
2024-06-25 15:04:08 +0100

    Fleshed out the player1 placement screen

--- src/DOM.js
+++ src/DOM.js
@@ -7,6 +7,21 @@ function buttonClicker() {
     secondPlayer.style.display = "none";
     inputSecondPlayer.style.display = "inline-block";
   });
+  const startBattle = document.getElementById("submit");
+  const launchScreen = document.getElementById("launch-screen");
+  const player2Name = document.getElementById("player2");
+  startBattle.addEventListener("click", () => {
+    event.preventDefault();
+    launchScreen.style.display = "none";
+    if (player2Name.value !== null) {
+      singlePlayerPlacement();
+    } else twoPlayerPlacement();
+  });
+}
+
+function singlePlayerPlacement() {
+  const playerPlacementScreen = document.getElementById("player1-placement");
+  playerPlacementScreen.style.display = "grid";
 }
 
 export { buttonClicker };
